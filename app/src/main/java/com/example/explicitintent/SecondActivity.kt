aab65f27ae97442a3e21953904b916fa5973943b
SaurabhIndi <saurabhyadavaligarh@gmail.com>
2024-03-11 21:31:33 +0530

    Adding Files of the Project

---
+++ app/src/main/java/com/example/explicitintent/SecondActivity.kt
@@ -0,0 +1,33 @@
+package com.example.explicitintent
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+
+
+        val buttonIntent= findViewById<Button>(R.id.button2)
+
+        buttonIntent.setOnClickListener{
+            //open an other activity
+            intent= Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
